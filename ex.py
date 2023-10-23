5f69fa95514d94f5629eb9b8e7874c2688f66ffb
mrrahbarnia <mrrahbarnia@gmail.com>
2023-10-23 11:54:05 +0330

    Updating and deleting records by selecting them with joins,also used postgresql instead of sqlite in docker container.

--- ex.py
+++ ex.py
@@ -6,54 +6,20 @@ from sqlparse import format
 from ecommerce.inventory.models import Brand, ProductInventory, Product, Category, Stock, ProductAttribute
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
-x = Brand.objects.all().query
-print(x)
+# Updating
+Brand.objects.filter(id__range=(1, 5)).update(name="NewBrand")
+Brand.objects.update_or_create(name='Nothing')
+Brand.objects.update_or_create(name='Nothing', defaults={'name': 'edited'})
 
-cursor = connection.cursor()
-x = cursor.execute('SELECT * FROM inventory_brand')
+data = [(1, 'c'), (2, 'd')]
+data_ids = [id for id, name in data]
+updating_objects = Brand.objects.filter(id__in=data_ids)
+update_list = []
+for obj in updating_objects:
+    obj.name = next(name for id, name in data if id==obj.id)
+    update_list.append(obj)
 
-x = Brand.objects.raw('SELECT * FROM inventory_brand')
+Brand.objects.bulk_update(update_list, ['name'])
 
-try:
-    x = Brand.objects.get(id=15)
-    print(x.name)
-except ObjectDoesNotExist:
-    print("Does not exist")
-
-x = Brand.objects.raw("SELECT * FROM inventory_brand WHERE id=1 OR id=2")
-for i in x:
-    print(i.id, i.name)
-
-x = Brand.objects.filter(id=1)
-x = Brand.objects.filter(id=1, name='361')
-x = Brand.objects.filter(id=1, name='361') | Brand.objects.filter(id=2)
-x = Brand.objects.filter(id__gt=1)
-x = Brand.objects.filter(id__gte=1)
-x = Brand.objects.filter(id__lt=2)
-x = Brand.objects.filter(id__lte=2)
-x = Brand.objects.filter(name__startswith='a')
-x = Brand.objects.exclude(name__startswith='a')
-
-x = Brand.objects.raw("SELECT * FROM inventory_brand WHERE id=1")
-x = Brand.objects.raw("SELECT * FROM inventory_brand WHERE id=1 AND name='361'")
-x = Brand.objects.raw("SELECT * FROM inventory_brand WHERE id=1 AND name='361' OR id=2")
-x = Brand.objects.raw("SELECT * FROM inventory_brand WHERE id=1 AND name='361' OR id=2 OR NOT id=8")
-
-ProductInventory.objects.filter(brand__name='a.x.n.y.')
-Product.objects.filter(category__name='boots')
-Category.objects.filter(category__name='lips too too effort tan anke boot')
-ProductInventory.objects.filter(product__category__name='heels')
-Category.objects.filter(category__product__id=12)
-
-x = ProductInventory.objects.raw("SELECT * FROM inventory_productinventory INNER JOIN inventory_product ON inventory_productinventory.product_id=inventory_product.id INNER JOIN inventory_category ON inventory_product.category_id=inventory_category.id WHERE inventory_category.name='heels'")
-
-formatted = format(str(x.query), reindent=True)
-print(highlight(formatted, PostgresLexer(), TerminalFormatter()))
-
-ProductInventory.objects.filter(product_inventory__units__gt='50')
-Stock.objects.filter(product_inventory__sku__gt=6327000212)
-
-ProductInventory.objects.filter(attribute_values__product_attribute__name="woman-shoe-size")
-ProductAttribute.objects.filter(product_attribute__product_attribute_values__id=1)
-ProductAttribute.objects.filter(product_type_attributes__product_type__id=1)
-ProductInventory.objects.filter(attribute_values__product_attribute__name='woman-shoe-size')
+# Deleting
+Brand.objects.all().delete()
